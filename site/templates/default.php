95e9d105820787bc3ba3b9c8f57911f7c54ad3fe
Johannes Isphording <mail@johannesisphording.de>
2020-12-16 19:47:20 +0100

    [template][content] added to default template

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,2 +1,10 @@
+<!-- Fetching the header snippet -->
+<?php snippet('header') ?>
+
 <h1><?= $page->title() ?></h1>
 <p><?= $page->Text()->kirbytext() ?></p>
+
+<!-- Get all images from news item -->
+<?php foreach( $page -> images() as $file ): ?>
+    <img src="<?= $file->url() ?>" alt="Cover Bild f&uuml;r den Newseintrag" width="<?= floor($file->width()) ?>" height="<?= floor($file->height()) ?>">
+<?php endforeach ?>

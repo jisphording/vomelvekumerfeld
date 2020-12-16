3e3f20d67c4a63dd42d553028621a2c804bc6eed
Johannes Isphording <mail@johannesisphording.de>
2020-12-16 19:48:29 +0100

    [template][setup] adding basic menu snippets

---
+++ site/snippets/submenu.php
@@ -0,0 +1,27 @@
+<!-- Sub Menu -->
+<div class="menu menu__sub">
+
+    <?php
+    $items = false;
+
+    // get the open item on the first level
+    if($root = $pages->findOpen()) {
+
+    // get visible children for the root item
+    $items = $root->children()->listed();
+    }
+
+    // only show the menu if items are available
+    if($items and $items->isNotEmpty()):
+
+    ?>
+    <nav>
+    <ul>
+        <?php foreach($items as $item): ?>
+        <li><a<?php e($item->isOpen(), ' class="active"') ?> href="<?= $item->url() ?>"><?= $item->title()->html() ?></a></li>
+        <?php endforeach ?>
+    </ul>
+    </nav>
+    <?php endif ?>
+
+</div>

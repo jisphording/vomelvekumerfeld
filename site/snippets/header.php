d32997c07cd2bb429683661e78a416aa699384a9
Johannes Isphording <mail@johannesisphording.de>
2020-12-16 19:49:04 +0100

    [template][setup] Adding basic header snippet

---
+++ site/snippets/header.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en" class="white-bg">
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <meta name="description" content="Online Presence of Dog Kennel and Breeder Vom Elvekumer Feld." />
+    <meta name="author" content="Johannes Isphording" />
+    <meta name="keywords" content="dogs, golden retriever, breeder, kennel, dog training" />
+
+    <title><?= $site->title() ?> | <?= $page->title() ?></title>
+
+    <!-- Canonical Link to main content for this site -->
+    <link rel="canonical" href="https://vomelvekumerfeld.test/">
+
+    <!-- main css file for styling -->
+    <?= css(['dist/index.css']) ?>
+
+</head>
+<body class="loading">
+
+  <div class="page <?= $page->uri() ?>">
+    <header class="header">
+
+		<section id="navbar" class="navbar">
+            <h1 class="logo logotype">Vom Elvekumer Feld</h1>
+
+            <!-- include the main menu -->
+            <?php snippet('menu') ?>
+
+            <!-- include submenu only for selected pages -->
+            <?php if ($page->slug() == 'hunde' || 'zucht') {
+                snippet('submenu'); 
+            } ?> 
+
+		</section>
+
+    </header>
+

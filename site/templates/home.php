ff4c7c4e69ce62949763e5e10b86ce0bb5361ff0
Johannes Isphording <mail@johannesisphording.de>
2021-01-13 15:15:20 +0100

    [php] initialize custom homepage template

---
+++ site/templates/home.php
@@ -0,0 +1,23 @@
+<!-- Fetching the header snippet -->
+<?php snippet('header') ?>
+<!-- Fetching the standard mood image -->
+<?php snippet('intro-mood') ?>
+
+<!-- List all subpages on page - makes for easier backend management -->
+
+<!-- Main Page Loop -->
+<section class="content content__main">
+    <?= $page->content()->text()->blocks(); ?>
+</section>
+
+<!-- Image Loop -->
+<?php $images = $page->images();
+foreach ($images as $image):
+    echo $image;
+endforeach; ?>
+
+<!-- Fetching the standard mood image -->
+<?php snippet('insert-readmore') ?>
+
+<!-- Fetching the footer snippet -->
+<?php snippet('footer') ?>

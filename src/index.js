772f387d8797daa4c879a154ce3b9a5c51f5d36d
jisphording <mail@johannesisphording.de>
2022-11-10 23:24:57 +0100

    [setup] small changes to entry point

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
+// ---------- ---------- ---------- ---------- ---------- //
+// A P P   E N T R Y   P O I N T //
+// ---------- ---------- ---------- ---------- ---------- //
+//
 // This is the main JavaScript file to enhance the Webflow Site.
-alert('Willkommen auf dem Elvekumer Feld!')
+
+// Import Cookie Consent
+import CookieConsent from './modules/CookieConsent.mjs'
 
 // Code of main.js
 console.log('Code of main.js goes below.')

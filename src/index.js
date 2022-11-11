357c600223371a534200a45ee3cbb19296440e2c
Johannes Isphording <mail@johannesisphording.de>
2022-11-11 15:01:44 +0100

    [setup] added the strip script to index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@
 //
 // This is the main JavaScript file to enhance the Webflow Site.
 
+// Strip unused Webflow Scripts
+import stripScripts from './utils/stripscripts.mjs'
+
 // Import Cookie Consent
 import CookieConsent from './modules/CookieConsent.mjs'
 
